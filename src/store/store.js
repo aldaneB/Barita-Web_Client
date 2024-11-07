012c12911c31ee5be23321108c934484b11fbc42
truedigital <aldane.barrett@mymona.uwi.edu>
2024-11-07 14:09:00 -0500

    Register authReducer

--- src/store/store.js
+++ src/store/store.js
@@ -5,10 +5,12 @@
 
 import { configureStore } from "@reduxjs/toolkit";
 import transactionReducer from "./transaction-slice";
+import authReducer from "./auth-slice";
 
 const store = configureStore({
   reducer: {
     transaction: transactionReducer,
+    auth: authReducer,
   },
 });
 
