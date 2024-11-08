f89fefe707c30c08e10ba4eb2c9e5ca52c121a02
truedigital <aldane.barrett@mymona.uwi.edu>
2024-11-07 20:53:44 -0500

    Update states

--- src/store/auth-slice/index.js
+++ src/store/auth-slice/index.js
@@ -6,15 +6,18 @@ import axios from "axios";
 const initialState = {
   isAuthenticated: false,
   isLoading: false,
-  token: null,
+  error: null,
 };
 
+/**
+ *
+ */
 export const loginUser = createAsyncThunk(
   "/api/v1/Auth/login",
   async (formData, { rejectWithValue }) => {
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/v1/Auth/login",
+        "https://localhost:7075/api/v1/Auth/login",
         formData
       );
       localStorage.setItem("token", response.data.token);
@@ -29,8 +32,8 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    loginSuccess(state, action) {
-      state.token = action.payload.token;
+    loginSuccess(state) {
+      state.isAuthenticated = true;
     },
   },
   extraReducers: (builder) => {
@@ -38,14 +41,14 @@ const authSlice = createSlice({
       .addCase(loginUser.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(loginUser.fulfilled, (state, action) => {
+      .addCase(loginUser.fulfilled, (state) => {
         state.isLoading = false;
         state.isAuthenticated = true;
-        state.token = action.payload.token;
       })
-      .addCase(loginUser.rejected, (state) => {
+      .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         state.isAuthenticated = false;
+        state.error = action.payload.error;
       });
   },
 });
